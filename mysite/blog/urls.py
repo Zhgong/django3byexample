d18a2217b32943e8b2c92127a98c179e16f1204e
zhou <zhou@ubuntu.com>
2021-07-10 21:48:26 +0200

    发送邮件

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,8 +8,10 @@ urlpatterns = [
     # path("", views.post_list, name="post_list"),
     path("", views.PostListView.as_view(), name="post_list"),
    
-
     path("<int:year>/<int:month>/<int:day>/<slug:post>/",
          views.PostDetailView.as_view(), name="post_detail"),
+
+    path("<int:post_id>/share/", views.post_share, name="post_share"),
+    
 
 ]
