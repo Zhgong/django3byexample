332be8814045d8ee7fb30e0f858464b0eb0f6961
zhou <zhou@ubuntu.com>
2021-08-05 23:14:04 +0200

    查询词语词干化，对查询结果排名

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,7 +4,7 @@ from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.views.generic.base import TemplateView
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
@@ -119,8 +119,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),).filter(search=query) # 传回搜索结果
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank') # 传回搜索结果
     return render(request,
                   'blog/post/search.html',
                   {'form': form,
